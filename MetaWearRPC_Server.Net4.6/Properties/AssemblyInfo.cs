5ee3ffd336fe7306e0bb1ab2d3fa5b6a462e2d47
ebadier <emmanuel.badier@gmail.com>
2018-12-11 18:18:14 +0100

    - .Net4.6 Still doesn't work

--- MetaWearRPC_Server.Net4.6/Properties/AssemblyInfo.cs
+++ MetaWearRPC_Server.Net4.6/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("MetaWearRPCServer2")]
+[assembly: AssemblyTitle("MetaWearRPCServer.Net4.6")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MetaWearRPCServer2")]
+[assembly: AssemblyProduct("MetaWearRPCServer.Net4.6")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
